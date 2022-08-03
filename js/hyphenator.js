0d6cb03899ed2bec439410a0251ee1c24f5140c8
MJ Brechner <mjbrechner@gmail.com>
2022-08-02 20:44:03 -0400

    all hyphens/dashes/blanks now get properly hyphenated.

--- js/hyphenator.js
+++ js/hyphenator.js
@@ -6,11 +6,11 @@ function hyphenator() {
     e = document.getElementById("original-text").value;
 
     if (e != "") {
-        document.getElementById("results-no-dash").textContent = e.replaceAll('-', ' ');
-        document.getElementById("results-hyphen").textContent = e;
-        document.getElementById("results-figure-dash").textContent = e.replaceAll('-', '‒');
-        document.getElementById("results-en-dash").textContent = e.replaceAll('-', '–');
-        document.getElementById("results-quotation-dash").textContent = e.replaceAll('-', '―');
-        document.getElementById("results-em-dash").textContent = e.replaceAll('-', '—');
+        document.getElementById("results-no-dash").textContent = e.replace(/[\s-‒–―—]/g, ' ');
+        document.getElementById("results-hyphen").textContent = e.replace(/[\s-‒–―—]/g, '-');;
+        document.getElementById("results-figure-dash").textContent = e.replace(/[\s-‒–―—]/g, '‒');
+        document.getElementById("results-en-dash").textContent = e.replace(/[\s-‒–―—]/g, '–');
+        document.getElementById("results-quotation-dash").textContent = e.replace(/[\s-‒–―—]/g, '―');
+        document.getElementById("results-em-dash").textContent = e.replace(/[\s-‒–―—]/g, '—');
     }
 }
