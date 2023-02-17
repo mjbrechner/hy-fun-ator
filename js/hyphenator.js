ed58b8ed8820c27eba485ac7aec03cef489dc191
MJ Brechner <mjbrechner@gmail.com>
2023-02-16 20:30:46 -0500

    added text decoration to highlight the most recent line copied to clipboard

--- js/hyphenator.js
+++ js/hyphenator.js
@@ -1,10 +1,22 @@
 'use strict';
 
+// Getting rid of text decoration, for when the hyphenator is active OR when the COPIED! message pops up
+function removeTextDecoration() {
+    document.getElementById("results-no-dash").style.textDecoration = "none";
+    document.getElementById("results-hyphen").style.textDecoration = "none";
+    document.getElementById("results-figure-dash").style.textDecoration = "none";
+    document.getElementById("results-en-dash").style.textDecoration = "none";
+    document.getElementById("results-quotation-dash").style.textDecoration = "none";
+    document.getElementById("results-em-dash").style.textDecoration = "none";
+}
+
+// Running the actual hyphenator
 function hyphenator() {
     let e;
     e = document.getElementById("original-text").value;
 
     if (e !== "") {
+        removeTextDecoration();
         document.getElementById("results-no-dash").textContent = e.replace(/[-‒–―—]/g, ' ');
         document.getElementById("results-hyphen").textContent = e.replace(/[-‒–―—]/g, '-');;
         document.getElementById("results-figure-dash").textContent = e.replace(/[-‒–―—]/g, '‒');
--- js/textcopier.js
+++ js/textcopier.js
@@ -8,49 +8,55 @@ let resultsQuotationDashContents;
 let resultsEmDashContents;
 
 // Returning appearance to normal after COPY message appears
-
+// Adding text decoration to highlight the most recent line that was copied
 function copiedReturnToNormalNoDash() {
     document.getElementById("results-no-dash").textContent = resultsNoDashContents;
     document.getElementById("results-no-dash").style.backgroundColor = "#DCD7C9";
     document.getElementById("results-no-dash").style.color = "#3F4E4F";
+    document.getElementById("results-no-dash").style.textDecoration = "underline #A27B5C";
 }
 
 function copiedReturnToNormalHyphen() {
     document.getElementById("results-hyphen").textContent = resultsHyphenContents;
     document.getElementById("results-hyphen").style.backgroundColor = "#DCD7C9";
     document.getElementById("results-hyphen").style.color = "#3F4E4F";
+    document.getElementById("results-hyphen").style.textDecoration = "underline #A27B5C";
 }
 
 function copiedReturnToNormalFigureDash() {
     document.getElementById("results-figure-dash").textContent = resultsFigureDashContents;
     document.getElementById("results-figure-dash").style.backgroundColor = "#DCD7C9";
     document.getElementById("results-figure-dash").style.color = "#3F4E4F";
+    document.getElementById("results-figure-dash").style.textDecoration = "underline #A27B5C";
 }
 
 function copiedReturnToNormalEnDash() {
     document.getElementById("results-en-dash").textContent = resultsEnDashContents;
     document.getElementById("results-en-dash").style.backgroundColor = "#DCD7C9";
     document.getElementById("results-en-dash").style.color = "#3F4E4F";
+    document.getElementById("results-en-dash").style.textDecoration = "underline #A27B5C";
 }
 
 function copiedReturnToNormalQuotationDash() {
     document.getElementById("results-quotation-dash").textContent = resultsQuotationDashContents;
     document.getElementById("results-quotation-dash").style.backgroundColor = "#DCD7C9";
     document.getElementById("results-quotation-dash").style.color = "#3F4E4F";
+    document.getElementById("results-quotation-dash").style.textDecoration = "underline #A27B5C";
 }
 
 function copiedReturnToNormalEmDash() {
     document.getElementById("results-em-dash").textContent = resultsEmDashContents;
     document.getElementById("results-em-dash").style.backgroundColor = "#DCD7C9";
     document.getElementById("results-em-dash").style.color = "#3F4E4F";
+    document.getElementById("results-em-dash").style.textDecoration = "underline #A27B5C";
 }
 
 // Copying contents to clipboard and flashing notifications
-
 function resultsNoDashCopier() {
     if (document.getElementById("results-no-dash").textContent != "COPIED!") {
         resultsNoDashContents = document.getElementById("results-no-dash").textContent;
         navigator.clipboard.writeText(resultsNoDashContents);
+        removeTextDecoration();
         document.getElementById("results-no-dash").textContent = "COPIED!";
         document.getElementById("results-no-dash").style.backgroundColor = "#3F4E4F";
         document.getElementById("results-no-dash").style.color = "#DCD7C9";
@@ -62,6 +68,7 @@ function resultsHyphenCopier() {
     if (document.getElementById("results-hyphen").textContent != "COPIED!") {
         resultsHyphenContents = document.getElementById("results-hyphen").textContent;
         navigator.clipboard.writeText(resultsHyphenContents);
+        removeTextDecoration();
         document.getElementById("results-hyphen").textContent = "COPIED!";
         document.getElementById("results-hyphen").style.backgroundColor = "#3F4E4F";
         document.getElementById("results-hyphen").style.color = "#DCD7C9";
@@ -73,6 +80,7 @@ function resultsFigureDashCopier() {
     if (document.getElementById("results-figure-dash").textContent != "COPIED!") {
         resultsFigureDashContents = document.getElementById("results-figure-dash").textContent;
         navigator.clipboard.writeText(resultsFigureDashContents);
+        removeTextDecoration();
         document.getElementById("results-figure-dash").textContent = "COPIED!";
         document.getElementById("results-figure-dash").style.backgroundColor = "#3F4E4F";
         document.getElementById("results-figure-dash").style.color = "#DCD7C9";
@@ -84,6 +92,7 @@ function resultsEnDashCopier() {
     if (document.getElementById("results-en-dash").textContent != "COPIED!") {
         resultsEnDashContents = document.getElementById("results-en-dash").textContent;
         navigator.clipboard.writeText(resultsEnDashContents);
+        removeTextDecoration();
         document.getElementById("results-en-dash").textContent = "COPIED!";
         document.getElementById("results-en-dash").style.backgroundColor = "#3F4E4F";
         document.getElementById("results-en-dash").style.color = "#DCD7C9";
@@ -95,6 +104,7 @@ function resultsQuotationDashCopier() {
     if (document.getElementById("results-quotation-dash").textContent != "COPIED!") {
         resultsQuotationDashContents = document.getElementById("results-quotation-dash").textContent;
         navigator.clipboard.writeText(resultsQuotationDashContents);
+        removeTextDecoration();
         document.getElementById("results-quotation-dash").textContent = "COPIED!";
         document.getElementById("results-quotation-dash").style.backgroundColor = "#3F4E4F";
         document.getElementById("results-quotation-dash").style.color = "#DCD7C9";
@@ -107,6 +117,7 @@ function resultsEmDashCopier() {
     if (document.getElementById("results-em-dash").textContent != "COPIED!") {
         resultsEmDashContents = document.getElementById("results-em-dash").textContent;
         navigator.clipboard.writeText(resultsEmDashContents);
+        removeTextDecoration();
         document.getElementById("results-em-dash").textContent = "COPIED!";
         document.getElementById("results-em-dash").style.backgroundColor = "#3F4E4F";
         document.getElementById("results-em-dash").style.color = "#DCD7C9";
