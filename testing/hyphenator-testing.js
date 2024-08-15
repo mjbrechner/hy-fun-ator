0649d95cf6f7c8671944e273e409b98c0946360f
MJ Brechner <mjbrechner@gmail.com>
2024-08-15 15:49:52 -0400

    added in-progress testing option combining both html pages

---
+++ testing/hyphenator-testing.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const originalTextHyphenator = document.getElementById("original-text");
+const title = document.getElementById("title");
+const title2 = document.getElementById("title-2");
+const infoText = document.getElementById("info-text");
+const changeModeButton = document.getElementById("change-mode-button");
+let currentMode = "hyphenate";
+
+// Getting rid of text decoration, for when the hyphenator is active OR when the COPIED! message pops up
+function removeTextDecoration() {
+    document.getElementById("results-no-dash").style.textDecoration = "none";
+    document.getElementById("results-hyphen").style.textDecoration = "none";
+    document.getElementById("results-figure-dash").style.textDecoration = "none";
+    document.getElementById("results-en-dash").style.textDecoration = "none";
+    document.getElementById("results-quotation-dash").style.textDecoration = "none";
+    document.getElementById("results-em-dash").style.textDecoration = "none";
+    document.getElementById("results-hyphen2").style.textDecoration = "none";
+    document.getElementById("results-nospace").style.textDecoration = "none";
+
+    document.getElementById("results-typewriter").style.textDecoration = "none";
+    document.getElementById("results-typographic").style.textDecoration = "none";
+}
+
+// Running the hyphenator/apostrophenator
+function hyphenator() {
+    let e;
+    e = originalTextHyphenator.value;
+
+    if (e !== "") {
+        removeTextDecoration();
+        document.getElementById("results-no-dash").textContent = e.replace(/[-‒–―—‐]/g, ' ');
+        document.getElementById("results-hyphen").textContent = e.replace(/[-‒–―—‐]/g, '-');;
+        document.getElementById("results-figure-dash").textContent = e.replace(/[-‒–―—‐]/g, '‒');
+        document.getElementById("results-en-dash").textContent = e.replace(/[-‒–―—‐]/g, '–');
+        document.getElementById("results-quotation-dash").textContent = e.replace(/[-‒–―—‐]/g, '―');
+        document.getElementById("results-em-dash").textContent = e.replace(/[-‒–―—‐]/g, '—');
+        document.getElementById("results-hyphen2").textContent = e.replace(/[-‒–―—‐]/g, '‐');
+        document.getElementById("results-nospace").textContent = e.replace(/[-‒–―—‐]/g, '');
+
+        document.getElementById("results-typewriter").textContent = e.replace(/['’]/g, `'`);
+        document.getElementById("results-typographic").textContent = e.replace(/['’]/g, '’');;
+    }
+}
+
+// Enter key activating the button press
+originalTextHyphenator.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementById("hyphenator-button").click();
+    }
+});
+
+function changeMode() {
+    if (currentMode === "hyphenate") {
+        currentMode = "apostrophenate";
+        title.innerText ="Apostrophe'nator";
+        title2.innerText = "explore varieties of apostrophes";
+        infoText.innerText = "Want to see your text displayed with multiple versions of the apostrophe? Type something into the box and click the button to transform your existing text.";
+        changeModeButton.innerText = "Hy-fun-ator";
+    } else if (currentMode === "apostrophenate") {
+                currentMode = "hyphenate";
+                        title.innerText ="Hy-fun-ator";
+        title2.innerText = "putting the fun into hyphenation";
+        infoText.innerText = "Want to see your hyphenated text with all kinds of possible dashes? Type something into the box and click the button to transform your existing hyphenation into a smorgasbord of dashes.";
+        changeModeButton.innerText = "Apostrophe'nator";
+    }
+}
